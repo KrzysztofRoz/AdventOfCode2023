40ceec4020dcd63be960e6166884d0d92d6d227a
Krzysztof Rozbicki <rozkrzys@wp.pl>
2023-12-03 23:12:38 +0100

    Add second part to solution

--- day03/day3.go
+++ day03/day3.go
@@ -32,15 +32,42 @@ func (symbol *Symbol) Show() string {
 }
 
 func main() {
-	path := "/home/krzysztof/Repos/AdventOfCode2023/inputs/input03.txt"
-	fmt.Println(FirstPart(path))
-	//fmt.Println(SecondPart(path))
+	//path := "/home/krzysztof/Repos/AdventOfCode2023/inputs/input03.txt"
+	//fmt.Println(FirstPart(path))
+	fmt.Println(SecondPart("/home/krzysztof/Repos/AdventOfCode2023/inputs/input03.txt"))
 }
 
 func SecondPart(filePath string) int {
 
-	return 0
+	totalGearRatio := 0
+	engineParts := make([]EnginePart, 0)
+	symbols := make([]Symbol, 0)
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	i := 0
+	for scanner.Scan() {
+		engineParts = append(engineParts, parseEngineParts(scanner.Text(), i)...)
+		symbols = append(symbols, parseSymbols(scanner.Text(), i)...)
+		i++
+	}
+
+	for _, gear := range symbols {
+		if gear.mark == "*" {
+			totalGearRatio += calculateGearRatio(gear, engineParts)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return totalGearRatio
 }
+
 func FirstPart(filePath string) int {
 
 	partNumbersSum := 0
@@ -60,9 +87,7 @@ func FirstPart(filePath string) int {
 		symbols = append(symbols, parseSymbols(scanner.Text(), i)...)
 		i++
 	}
-	//fmt.Println(symbols)
-	//fmt.Println(engineParts)
-	fmt.Println("Parts:", len(engineParts), " and Symbols: ", len(symbols))
+
 	for _, part := range engineParts {
 		if partMatchSymbol(part, symbols) {
 			partNumbersSum += part.value
@@ -84,7 +109,6 @@ func parseSymbols(engineString string, line int) []Symbol {
 		symbol.line = line
 		symbol.index = match[0]
 		symbol.mark = engineString[match[0]:match[1]]
-		//fmt.Println(symbol.Show())
 		symbols = append(symbols, *symbol)
 	}
 
@@ -103,7 +127,6 @@ func parseEngineParts(engineString string, line int) []EnginePart {
 		enginePart.end = match[1] - 1
 		enginePart.value, err = strconv.Atoi(engineString[match[0]:match[1]])
 		engineParts = append(engineParts, *enginePart)
-		//fmt.Println(enginePart.Show())
 	}
 	if err != nil {
 		log.Fatal(err)
@@ -119,6 +142,56 @@ func partMatchSymbol(engPart EnginePart, symbols []Symbol) bool {
 			if engPart.start-1 <= symbol.index && engPart.end+1 >= symbol.index {
 				return true
 			}
+		}
+	}
+	return result
+}
+func calculateGearRatio(gear Symbol, engPart []EnginePart) int {
+	result := 0
+	var element1 *EnginePart
+	element1 = new(EnginePart)
+	var element2 *EnginePart
+	element2 = new(EnginePart)
+	var element3 *EnginePart
+	element3 = new(EnginePart)
+
+	for _, eng := range engPart {
+		//fmt.Println(gear.Show())
+		//fmt.Println(eng.Show())
+		if matchGear(gear, eng) {
+			fmt.Println("Match")
+			if element1.value == 0 {
+				fmt.Println("Insert to first")
+				element1.value = eng.value
+				fmt.Println(element1)
+			} else if element2.value == 0 {
+				fmt.Println("Insert to second")
+				element2.value = eng.value
+				fmt.Println(element2)
+
+			} else if element3.value == 0 {
+				fmt.Println("Insert to third")
+
+				element3.value = 1
+				break
+			}
+		}
+	}
+	if element3.value == 0 && element2.value != 0 {
+		result = element1.value * element2.value
+		fmt.Println("First element: ", element1.value, " Second element: ", element2.value)
+		fmt.Println("Add ", result)
+	}
+
+	return result
+}
+
+func matchGear(gear Symbol, engPart EnginePart) bool {
+	result := false
+
+	if gear.line-1 <= engPart.line && gear.line+1 >= engPart.line {
+		if (engPart.start <= gear.index+1 && engPart.start >= gear.index-1) || (engPart.end <= gear.index+1 && engPart.end >= gear.index-1) {
+			result = true
 		}
 	}
 	return result
