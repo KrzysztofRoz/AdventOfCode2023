24e69b48452db025d1b9746e78577ee81b6f2cd5
Krzysztof Rozbicki <rozkrzys@wp.pl>
2023-12-05 23:09:22 +0100

    Add solution to day4

--- day04/day4.go
+++ day04/day4.go
@@ -15,11 +15,56 @@ type ScrachCard struct {
 	Id          int
 	winNumbers  []int
 	cardNumbers []int
+	matches     int
+	instances   int
 }
 
 func main() {
 	path := "/home/krzysztof/Repos/AdventOfCode2023/inputs/input04.txt"
 	fmt.Println(FirstPart(path))
+	fmt.Println(SecondPart(path))
+}
+func SecondPart(filePath string) int {
+	//idea from top to bottom add instances for cards and then sum up from bottom to top
+	counter := 0
+	cards := make([]ScrachCard, 0)
+	totalCardsInstances := 0
+	file, err := os.Open(filePath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		cards = append(cards, ParseScrachCard(scanner.Text()))
+	}
+
+	for i, card := range cards {
+		counter = 0
+		for _, num := range card.winNumbers {
+			if Contains(card.cardNumbers, num) {
+				counter++
+			}
+		}
+		if counter != 0 {
+			cards[i].matches = counter
+		}
+	}
+	for i := range cards {
+
+		for j := i + 1; j <= i+cards[i].matches; j++ {
+			cards[j].instances += cards[i].instances
+		}
+	}
+	for i := range cards {
+		totalCardsInstances += cards[i].instances
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return totalCardsInstances
 }
 
 func FirstPart(filePath string) int {
@@ -68,7 +113,6 @@ func ParseScrachCard(cardString string) ScrachCard {
 	cardParts = strings.Split(cardString, ": ")
 	reg := regexp.MustCompile("(\\d+)")
 	card.Id, err = strconv.Atoi(reg.FindAllString(cardParts[0], -1)[0])
-	fmt.Println(card.Id)
 	lotery = strings.Split(cardParts[1], " | ")
 	for _, val := range reg.FindAllString(lotery[0], -1) {
 		temp, err = strconv.Atoi(val)
@@ -78,7 +122,7 @@ func ParseScrachCard(cardString string) ScrachCard {
 		temp, err = strconv.Atoi(val)
 		card.cardNumbers = append(card.cardNumbers, temp)
 	}
-	fmt.Println(card)
+	card.instances = 1
 	if err != nil {
 		log.Fatal(err)
 	}
